0dd287d1bdda960d99edc8795e2357a4e25ea93d
Aaron Ho <bicecreamaar@gmail.com>
2024-06-07 16:07:31 -0700

    fixed connection string

--- db/schema.sql
+++ db/schema.sql
@@ -5,29 +5,28 @@ USE employee_tracker_db;
 
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL
+    name VARCHAR(30)
 );
 
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     department_id INT,
-    title VARCHAR(30) NOT NULL,
+    title VARCHAR(30),
     salary DECIMAL,
     FOREIGN KEY (department_id)
-    REFERENCES dapartment(id)
-    ON DELETE SET NULL
+        REFERENCES department(id)
+        ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
     role_id INT,
     manager_id INT,
     FOREIGN KEY (role_id)
-    REFERENCES role(id)
-    ON DELETE SET NULL,
+        REFERENCES role(id),
     FOREIGN KEY (manager_id)
-    REFERENCES employee(id)
-    ON DELETE SET NULL,
+        REFERENCES employee(id)
+        ON DELETE SET NULL
 );
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 
 const db = mysql.createConnection(
     {
-        host: "localhost",
+        host: "127.0.0.1",
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
@@ -23,7 +23,7 @@ function menu() {
                     "View all deparments",
                     "View all roles",
                     "View all employees",
-                    "Add a deparment",
+                    "Add a department",
                     "Add a role",
                     "Add an employee",
                     "Update an employee's role",
@@ -31,5 +31,38 @@ function menu() {
                 ]
             }
         ]
-    )
+    ).then((answers) => {
+        switch(answers.menuInput) {
+            case "View all deparments":
+                return displayAllDepartments();
+            case "View all roles":
+                return displayAllRoles();
+            case "View all employees":
+                return displayAllEmployees();
+            case "Add a department":
+                return addDepartment();
+            case "Add a role":
+                return addRole();
+            case "Add an employee":
+                return addEmployee();
+            case "Update an employee's role":
+                return updateEmployee();
+            case "Exit":
+                process.exit();
+        }
+    });
 }
+
+function displayAllDepartments() {
+    return db
+        .promise()
+        .query("SELECT name, id FROM department")
+        .then((data) => {
+            console.log(data);
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+}
+
+menu();
